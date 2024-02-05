bf019672d7a80c49b844a54571ca9807807022ab
leejy811 <lee38956162@gmail.com>
2024-02-06 03:06:02 +0900

    init PBD Engine

---
+++ GPU_Cloth/GPU_Cloth/Face.h
@@ -0,0 +1,32 @@
+#ifndef __FACE_H__
+#define __FACE_H__
+
+#pragma once
+#include "Vertex.h"
+#include <vector>
+
+using namespace std;
+
+class Face
+{
+public:
+	int				_index;
+	Vec3<double>	_normal;
+	vector<Vertex*>	_vertices; // Triangle : num. vertex -> 3
+	vector<double>	_structuralLength; // 0 : 0-1, 1 : 1-2, 2 : 0-2
+	vector<double>	_bendLength;
+public:
+	Face();
+	Face(int index, Vertex* v0, Vertex* v1, Vertex* v2)
+	{
+		_index = index;
+		_vertices.push_back(v0);
+		_vertices.push_back(v1);
+		_vertices.push_back(v2);
+	}
+	~Face();
+public:
+	int		getIndex(Vertex* v);
+};
+
+#endif
